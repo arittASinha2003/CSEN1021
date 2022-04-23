38c3ee2970c2a4f3b67a75ecfa5b938d8b149f21
arittASinha2003 <93367827+arittASinha2003@users.noreply.github.com>
2022-04-23 08:34:58 +0000

    Python

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-#print("Hello AS!")
+print("Hello AS!")
+print("Let's create something beautiful with code :-)")
 # https://gitam-2021.github.io/CSEN1021/
 # https://www.w3schools.com/python/python_ref_string.asp
+# Python Projects: https://www.youtube.com/watch?v=8ext9G7xspg
