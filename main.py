c575ed93e471bd42abf7c0e41eb45574c0c74d9f
arittASinha2003 <93367827+arittASinha2003@users.noreply.github.com>
2022-04-23 11:45:33 +0000

    Python

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ print("Hello AS!")
 print("Let's create something beautiful with code :-)")
 # https://gitam-2021.github.io/CSEN1021/
 # https://www.w3schools.com/python/python_ref_string.asp
-# Python Projects: https://www.youtube.com/watch?v=8ext9G7xspg
+
 # https://replit.com/@CoderGautamYT/swordbattleio-UPDATED?v=1
 # https://replit.com/@d12z/image-generator?v=1
+
+# Python Projects: https://www.youtube.com/watch?v=8ext9G7xspg
+# https://stackoverflow.com/questions/594273/how-to-pick-a-random-english-word-from-a-list
