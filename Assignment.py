5d44c794e10d78412c4f18709b0240d237fd47b9
arittASinha2003 <93367827+arittASinha2003@users.noreply.github.com>
2022-05-07 11:33:18 +0000

    Python

---
+++ Assignment.py
@@ -0,0 +1,10 @@
+def my_details(S):
+  for key in S:
+    print(key, ":", S[key])
+
+# S = {"Name" : "Aritta Sinha", "Roll Number" : "VU21CSEN0100997", "Branch" : "CSE", "Specialization" : "Core", "Email Id" : "asinha2@gitam.in", "Moodle Id" : "Aritta Sinha 210061751", "Github Id" : "arittASinha2003", "Replit Id" : "arittASinha2003", "Semester" : "2", "Year" : "1"}
+S = {"Name" : {"First Name" : "Aritta", "Last Name" : "Sinha"}, "Roll Number" : "VU21CSEN0100997", "Branch" : "CSE", "Specialization" : "Core", "Email Id" : "asinha2@gitam.in", "Moodle Id" : "Aritta Sinha 210061751", "Github Id" : "arittASinha2003", "Replit Id" : "arittASinha2003", "Semester" : "2", "Year" : "1"}
+
+dt = my_details(S)
+print(dt)
+# my_details(S)
--- Module-02/Dictionary.py
+++ Module-02/Dictionary.py
@@ -1,8 +1,8 @@
 person1 = {"Name" : "Aritt", "Age" : 18}
 
 print(person1)
-print(person1["Age"])
 print(person1["Name"])
+print(person1["Age"])
 # print(person1["Hobbies"])
 
 print("\n")
@@ -10,14 +10,15 @@ person1["Name"] = "AS"  # Changes previous value
 print(person1)
 
 print("\n")
-person1["Hobbies"] = ["Dancing", "Fishing"]  # Adds new key values
+person1["Hobbies"] = "Dancing, Fishing"  # Adds new key values
 print(person1)
 
-print("\n")
-print(person1.pop("Name"))  # Displays Name
-print(person1)  # Removes Name
+# print("\n")
+# print(person1.pop("Name"))  # Displays Name
+# print(person1)  # Removes Name
 
 print("\n")
 for key in person1:
-  print(key)
-  print(person1[key])
+  # print(key)
+  # print(person1[key])
+  print(key, ":", person1[key])
